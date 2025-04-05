4e1cfb4a91c29ce12d12c4a0df86a97019e63ac6
Raymond9734 <rymadara97@gmail.com>
2025-04-05 19:51:40 +0300

    refactor(cubes): simplify Cubes struct by removing color from cubes vector and adding a color field
    Changes:
    - Removed color parameter from the cubes vector, simplifying the data structure.
    - Introduced a color field in the Cubes struct to maintain color information.
    - Commented out the new method for future implementation, retaining the structure for random cube generation.
    
    Impact:
    - Enhances the clarity of the Cubes struct by associating color directly with the instance.
    - Streamlines the data structure for cube generation, making it more efficient and easier to manage.

--- src/geometrical_shapes/cubes.rs
+++ src/geometrical_shapes/cubes.rs
@@ -1,56 +1,52 @@
-use super::{Drawable, Point, Line};
-use crate::geometrical_shapes::Displayable;
-use raster::{Color, Image};
+use super::{Drawable, Line, Point};
+// use crate::geometrical_shapes::Displayable;
 use rand::Rng;
+use raster::{Color, Image};
 
 #[derive(Debug)]
 pub struct Cubes {
-    cubes: Vec<(Point, i32, Color)>,
+    cubes: Vec<(Point, i32)>,
+    color: Color,
 }
 
 impl Cubes {
-    pub fn new(position: &Point, size: i32) -> Self {
-        let mut rng = rand::thread_rng();
-        let mut cubes = Vec::new();
-        
-        let base_color = Color::rgb(
-            rng.gen_range(150..255),
-            rng.gen_range(150..255),
-            rng.gen_range(150..255),
-        );
+    // pub fn new(position: &Point, size: i32) -> Self {
+    //     let mut rng = rand::thread_rng();
+    //     let mut cubes = Vec::new();
 
-        for _ in 0..rng.gen_range(3..6) {
-            let offset_x = rng.gen_range(-size..size);
-            let offset_y = rng.gen_range(-size..size);
-            cubes.push((
-                Point::new(position.x + offset_x, position.y + offset_y),
-                rng.gen_range(size/2..size*3/2).max(20),
-                base_color.clone()
-            ));
-        }
-        
-        Cubes { cubes }
-    }
+    //     let color = Color::rgb(
+    //         rng.gen_range(150..255),
+    //         rng.gen_range(150..255),
+    //         rng.gen_range(150..255),
+    //     );
+
+    //     for _ in 0..rng.gen_range(3..6) {
+    //         let offset_x = rng.gen_range(-size..size);
+    //         let offset_y = rng.gen_range(-size..size);
+    //         cubes.push((
+    //             Point::new(position.x + offset_x, position.y + offset_y),
+    //             rng.gen_range(size / 2..size * 3 / 2).max(20),
+    //         ));
+    //     }
+
+    //     Cubes { cubes, color }
+    // }
 
     pub fn random(width: i32, height: i32) -> Self {
         let mut rng = rand::thread_rng();
         let mut cubes = Vec::new();
-        
-        let base_color = Color::rgb(
+
+        let color = Color::rgb(
             rng.gen_range(150..255),
             rng.gen_range(150..255),
             rng.gen_range(150..255),
         );
 
         for _ in 0..rng.gen_range(3..6) {
-            cubes.push((
-                Point::random(width, height),
-                rng.gen_range(30..80),
-                base_color.clone()
-            ));
+            cubes.push((Point::random(width, height), rng.gen_range(30..80)));
         }
-        
-        Cubes { cubes }
+
+        Cubes { cubes, color }
     }
 
     fn get_isometric_projection(center: &Point, size: i32) -> [Point; 8] {
@@ -59,43 +55,53 @@ impl Cubes {
         let s = size;
 
         [
-            Point::new(x - s, y - s/2),
-            Point::new(x + s, y - s/2),
-            Point::new(x - s, y + s/2),
-            Point::new(x + s, y + s/2),
-            Point::new(x - s/2, y - s),
-            Point::new(x + s/2, y - s),
-            Point::new(x - s/2, y),
-            Point::new(x + s/2, y)
+            Point::new(x - s, y - s / 2),
+            Point::new(x + s, y - s / 2),
+            Point::new(x - s, y + s / 2),
+            Point::new(x + s, y + s / 2),
+            Point::new(x - s / 2, y - s),
+            Point::new(x + s / 2, y - s),
+            Point::new(x - s / 2, y),
+            Point::new(x + s / 2, y),
         ]
     }
 }
 
 impl Drawable for Cubes {
     fn draw(&self, image: &mut Image) {
-        for (center, size, color) in &self.cubes {
+        for (center, size) in &self.cubes {
             let vertices = Cubes::get_isometric_projection(center, *size);
-            
+
             let edges = [
-                (0, 1), (1, 3), (3, 2), (2, 0),
-                (4, 5), (5, 7), (7, 6), (6, 4),
-                (0, 4), (1, 5), (2, 6), (3, 7)
+                (0, 1),
+                (1, 3),
+                (3, 2),
+                (2, 0),
+                (4, 5),
+                (5, 7),
+                (7, 6),
+                (6, 4),
+                (0, 4),
+                (1, 5),
+                (2, 6),
+                (3, 7),
             ];
 
             for (i, j) in edges.iter() {
                 let start = &vertices[*i];
                 let end = &vertices[*j];
-                
+
                 for offset in -1..=1 {
                     let adjusted_start = Point::new(start.x + offset, start.y + offset);
                     let adjusted_end = Point::new(end.x + offset, end.y + offset);
-                    Line::from_points(&adjusted_start, &adjusted_end).draw(image);
+                    let line = Line::from_points(&adjusted_start, &adjusted_end);
+                    line.draw(image);
                 }
             }
         }
     }
 
     fn color(&self) -> Color {
-        self.cubes[0].2.clone()
+        self.color.clone()
     }
 }
